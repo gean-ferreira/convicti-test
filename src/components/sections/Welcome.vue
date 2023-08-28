7d260f4a46cedaca9c7f928d304cb0006038cb2d
Gean Ferreira <gean_roberto@hotmail.com>
2023-08-28 18:45:01 -0300

    feat: Add a section de boas vindas ao usuário que acabou de logar

---
+++ src/components/sections/Welcome.vue
@@ -0,0 +1,29 @@
+<template>
+  <main class="welcome">
+    <h1 class="welcome__title">
+      Bem vindo, <strong>{{ username }}</strong
+      >!
+    </h1>
+  </main>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  // COMPUTED USERNAME STORE
+
+  const username = ref('Fulano')
+</script>
+
+<style lang="scss" scoped>
+  .welcome {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+
+    &__title {
+      font-size: 56px;
+    }
+  }
+</style>
