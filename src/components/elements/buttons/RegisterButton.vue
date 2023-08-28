eee299bd22348a7489dd5b8c68f28019b19e1ed8
Gean Ferreira <gean_roberto@hotmail.com>
2023-08-28 19:31:41 -0300

    feat: Add botão que aciona a funcionalidade Abrir o componente Offset

---
+++ src/components/elements/buttons/RegisterButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <button class="register">{{ name }} <Plus /></button>
+</template>
+
+<script setup>
+  import Plus from '@/components/icons/Plus.vue'
+  defineProps({
+    name: { default: 'Adicionar', type: String }
+  })
+</script>
+
+<style lang="scss" scoped>
+  .register {
+    width: fit-content;
+    padding: 24px 20px;
+    background-color: $BackgroundLight;
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  }
+</style>
