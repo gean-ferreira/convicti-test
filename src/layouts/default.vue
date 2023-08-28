4f0b9c6e6110acd98fa62102922b09943fdecf8f
Gean Ferreira <gean_roberto@hotmail.com>
2023-08-28 19:06:23 -0300

    feat: Cria o layout default da aplicação

---
+++ src/layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="app">
+    <Aside />
+    <section class="app__content">
+      <router-view></router-view>
+    </section>
+  </section>
+</template>
+
+<script setup>
+  import Aside from '@/components/sections/Aside.vue'
+</script>
+
+<style lang="scss" setup>
+  .app {
+    display: flex;
+    height: 100vh;
+
+    &__content {
+      background-color: $Background;
+      flex: 1;
+      padding: 32px;
+    }
+  }
+</style>
