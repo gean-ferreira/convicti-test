f3a8902aa09a0c9f701b4da4b0c91ed08bef5ae1
Gean Ferreira <gean_roberto@hotmail.com>
2023-08-28 18:03:19 -0300

    feat: Add componente LoginLogo

---
+++ src/components/logos/LoginLogo.vue
@@ -0,0 +1,12 @@
+<!-- image logo login -->
+<template>
+  <h1 class="logo">LOGO</h1>
+</template>
+
+<style lang="scss" scoped>
+  .logo {
+    font-size: 92px;
+    text-align: center;
+    color: white;
+  }
+</style>
