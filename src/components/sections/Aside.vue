cc5c8d0677f60a2489325748d6356b8e51cb40ae
Gean Ferreira <gean_roberto@hotmail.com>
2023-08-28 19:04:58 -0300

    feat: Add o component Aside

---
+++ src/components/sections/Aside.vue
@@ -0,0 +1,25 @@
+<template>
+  <aside class="aside">
+    <LoginLogo />
+    <Navbar />
+    <LogoutButton />
+  </aside>
+</template>
+
+<script setup>
+  import LoginLogo from '@/components/logos/LoginLogo.vue'
+  import Navbar from '@/components/sections/Navbar.vue'
+  import LogoutButton from '@/components/elements/buttons/LogoutButton.vue'
+</script>
+
+<style lang="scss" scoped>
+  .aside {
+    display: flex;
+    flex-direction: column;
+    background-color: $BackgroundPrimary;
+    align-items: center;
+    width: 350px;
+    padding: 48px 0;
+    gap: 88px;
+  }
+</style>
