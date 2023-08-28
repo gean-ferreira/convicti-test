aaeb117fc08cc3272511fbc493abe8688b1ba06c
Gean Ferreira <gean_roberto@hotmail.com>
2023-08-28 18:54:23 -0300

    feat: Add o botão que deslogará o usuário
    O botão utilizará a action logout no qual retirará todos os dados da store|cookie

---
+++ src/components/elements/buttons/LogoutButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <button @click="logout" class="logout"><Power /> Sair</button>
+</template>
+
+<script setup>
+  import Power from '@/components/icons/Power.vue'
+  import { useRouter } from 'vue-router'
+  const router = useRouter()
+  // STORE LOGOUT
+  const logout = () => router.push({ name: 'login' })
+</script>
+
+<style lang="scss" setup>
+  .logout {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    background-color: transparent;
+    color: white;
+  }
+</style>
