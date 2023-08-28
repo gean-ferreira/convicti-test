2b19b83b7f84bb30a9138c793e38036a85407456
Gean Ferreira <gean_roberto@hotmail.com>
2023-08-28 18:11:37 -0300

    feat: Add formulário Login

---
+++ src/components/forms/LoginForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <form @submit.prevent="submitForm" class="login-form">
+    <div class="login-form__field-container">
+      <label for="email">E-mail</label><br />
+      <input type="text" name="email" v-bind="" />
+    </div>
+    <div class="login-form__field-container">
+      <label for="password">Senha</label><br />
+      <input type="password" name="password" />
+    </div>
+    <button class="login-form__btn-submit secondary" type="submit">Entrar</button>
+  </form>
+</template>
+
+<script setup>
+  // USAR VEEVALIDATE FORM LIB
+  import { useRouter } from 'vue-router'
+
+  const router = useRouter()
+  const submitForm = () => {
+    router.push({ name: 'home' })
+  }
+</script>
+
+<style lang="scss" scoped>
+  .login-form {
+    &__field-container {
+      margin-bottom: 24px;
+
+      label {
+        color: white;
+      }
+    }
+
+    &__btn-submit {
+      width: 100%;
+      margin-top: 15px;
+    }
+  }
+</style>
--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,5 +1,6 @@
 import { createPinia } from 'pinia'
 import router from '@/router'
+// VEE VALIDADE
 
 export function applyPlugins(app) {
   app.use(createPinia())
