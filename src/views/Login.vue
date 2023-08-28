d6c6ef29d69fb226542de7fd2657a67cc9fba6ad
Gean Ferreira <gean_roberto@hotmail.com>
2023-08-28 18:40:53 -0300

    feat: atualiza estrutura e estilos da página Login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,31 @@
 <template>
-  <h1>Login</h1>
+  <div class="login">
+    <div class="login__container">
+      <LoginLogo />
+      <LoginForm />
+    </div>
+  </div>
 </template>
 
 <script setup>
+  import LoginLogo from '@/components/logos/LoginLogo.vue'
+  import LoginForm from '@/components/forms/LoginForm.vue'
 </script>
 
 <style lang="scss" setup>
+  .login {
+    height: 100vh;
+    background-color: $BackgroundPrimary;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &__container {
+      width: 100%;
+      max-width: 450px;
+      display: flex;
+      flex-direction: column;
+      gap: 92px;
+    }
+  }
 </style>
