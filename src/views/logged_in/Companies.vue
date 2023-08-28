68b99b1903612cc380a08464d3c0435869cf99d9
Gean Ferreira <gean_roberto@hotmail.com>
2023-08-28 20:23:45 -0300

    feat: atualiza estrutura da página Empresas

--- src/views/logged_in/Companies.vue
+++ src/views/logged_in/Companies.vue
@@ -1,9 +1,29 @@
 <template>
-  <h1>Empresas</h1>
+  <main class="companies">
+    <RegisterButton
+      @click="offsetStore.open('registerCompany')"
+      :name="'Adicionar Empresa'"
+      style="margin-left: auto"
+    />
+    <CompanieCard />
+    <Map />
+  </main>
 </template>
 
 <script setup>
+  import Map from '@/components/sections/Map.vue'
+  import RegisterButton from '@/components/elements/buttons/RegisterButton.vue'
+  import CompanieCard from '@/components/elements/cards/CompanieCard.vue'
+  import { useOffsetStore } from '@/stores/offsetStore.js'
+
+  const offsetStore = useOffsetStore()
 </script>
 
 <style lang="scss" scoped>
+  .companies {
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+    position: relative;
+  }
 </style>
